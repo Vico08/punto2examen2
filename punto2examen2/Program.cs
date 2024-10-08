327cad7326a47b1a96b41d3985ac44137a763fd8
Victoria Cano <victoriacano@outlook.es>
2024-10-07 22:41:30 -0500

    bloque de  while y for donde itera la cantidad de estudiantes

--- punto2examen2/Program.cs
+++ punto2examen2/Program.cs
@@ -6,4 +6,34 @@
         int estudiantesGanados = 0;
         int estudiantesPerdidos = 0;
 
-       
+        while (continuar)
+        {
+            Console.Clear();
+            Console.WriteLine("Sistema de Evaluación de Notas de Estudiantes");
+
+            int numEstudiantes = 0;
+            while (numEstudiantes <= 0)
+            {
+                Console.WriteLine("Ingrese el número de estudiantes:");
+                numEstudiantes = Convert.ToInt32(Console.ReadLine());
+                if (numEstudiantes <= 0)
+                {
+                    Console.WriteLine("Número de estudiantes inválido. Por favor, ingrese un número mayor que 0:");
+                }
+            }
+
+            for (int i = 0; i < numEstudiantes; i++)
+            {
+                Console.WriteLine($"Ingrese las notas del estudiante {i + 1} (separadas por espacios):");
+                string[] notasStr = Console.ReadLine().Split(' ');
+                double[] notas = new double[notasStr.Length];
+
+                for (int j = 0; j < notasStr.Length; j++)
+                {
+                    notas[j] = Convert.ToDouble(notasStr[j]);
+                }
+
+                double promedio = CalcularPromedio(notas);
+                string categoria = ObtenerCategoria(promedio);
+
+                Console.WriteLine($"Estudiante {i + 1}: Promedio = {promedio:F2}, Categoría = {categoria}");
