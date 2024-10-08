a9d09ff63f29a03568e10d27967554a821b5ee13
Victoria Cano <victoriacano@outlook.es>
2024-10-07 22:35:09 -0500

    variables

---
+++ punto2examen2/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        bool continuar = true;
+        int estudiantesGanados = 0;
+        int estudiantesPerdidos = 0;
+
+       
